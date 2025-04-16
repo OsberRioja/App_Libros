38364ab39f44bb08bf5a4772ec0d29d086469db5
Osber <osberriojalopez@gmail.com>
2025-04-15 20:59:43 -0400

    modelo del dominio

--- domain/src/main/java/com/ucb/domain/model/Book.kt
+++ domain/src/main/java/com/ucb/domain/model/Book.kt
@@ -1,4 +1,13 @@
 package com.ucb.domain.model
 
-class Book {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Book(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val authors: List<String>,
+    val publishYear: Int
+)
